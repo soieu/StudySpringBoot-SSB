642e47f383bfcb16de5143d2a5b91ded934f0bd8
soieu <soieu9898@gmail.com>
2025-08-27 17:49:30 +0900

    Initial commit

---
+++ src/test/java/demo/soieu/SoieuApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.soieu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoieuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
